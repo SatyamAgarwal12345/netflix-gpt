580d07ad8b51d18c9af9055f2eb6aa537415f93b
SatyamAgarwal12345 <satyamagarwal535@gmail.com>
2024-01-21 19:23:56 +0530

    made responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,35 +4,40 @@ import profile from "../assets/images/profile.jpg";
 import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import {  useDispatch, useSelector } from "react-redux";
-import { adduser ,removeUser} from "../utils/store/userSlice";
-
+import { useDispatch, useSelector } from "react-redux";
+import { adduser, removeUser } from "../utils/store/userSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
-  const user = useSelector(store=>store.user)
+  const user = useSelector((store) => store.user);
   const navigate = useNavigate();
+
   const signOutHandler = () => {
     signOut(auth)
       .then(() => {
-        dispatch(removeUser())
-        navigate("/")
+        dispatch(removeUser());
+        navigate("/");
       })
       .catch((error) => {});
   };
+
   return (
-    <div className="absolute w-screen flex justify-between px-8 py-2 bg-gradient-to-b from-black z-10">
-      <img className="w-28" src={logo}></img>
-      {user &&<div className="flex p-2">
-        <h1 className="m-2 font-bold text-2xl text-white">WELCOME {user.displayName}</h1>
-        <img className="w-12 h-12 mr-2" src={profile}></img>
-        <button
-          onClick={signOutHandler}
-          className="bg-red-500 rounded-2xl  px-2  text-white"
-        >
-          (Sign Out)
-        </button>
-      </div>}
+    <div className="fixed top-0 left-0 w-full flex justify-between items-center px-4 md:px-8 py-2 bg-gradient-to-b from-black z-10">
+      <img className="w-24 md:w-28" src={logo} alt="Netflix Logo" />
+      {user && (
+        <div className="flex items-center">
+          <h1 className="m-2 font-bold text-lg md:text-2xl text-white">
+            WELCOME {user.displayName}
+          </h1>
+          <img className="w-10 h-10 md:w-12 md:h-12 mr-2" src={profile} alt="Profile" />
+          <button
+            onClick={signOutHandler}
+            className="bg-red-500 rounded-2xl px-2 text-white text-sm md:text-base"
+          >
+            (Sign Out)
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -8,10 +8,9 @@ import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { adduser } from "../utils/store/userSlice";
+import {  signInWithEmailAndPassword } from "firebase/auth";
 
 
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { Link } from "react-router-dom";
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -23,112 +22,94 @@ const Login = () => {
   const email = useRef(null);
   const password = useRef(null);
   const name = useRef(null);
+
   const submitHandler = () => {
     const message = Validation(email.current.value, password.current.value);
-    console.log(message);
     setErrMsg(message);
+
     if (message) {
-      console.log("it is null error message");
       return;
     }
+
     if (!isSignIn) {
-      //signup
-      createUserWithEmailAndPassword(
-        auth,
-        email.current.value,
-        password.current.value
-      )
+      // signup
+      createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
         .then((userCredential) => {
-          // Signed up
           const user = userCredential.user;
           updateProfile(user, {
             displayName: name.current.value,
           }).then(() => {
-            const {uid,email,displayName} = auth.currentUser;
-            dispatch(adduser({uid:uid,email:email,displayName:displayName}))
-            navigate('/browse')
-          }).catch((error) => {
+            const { uid, email, displayName } = auth.currentUser;
+            dispatch(adduser({ uid: uid, email: email, displayName: displayName }));
+            navigate("/browse");
           });
-          console.log(user);
-          
-          // ...
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          // ..
-        });
-      console.log("auth sign Up");
-    } else {
-      //login
-
-      signInWithEmailAndPassword(
-        auth,
-        email.current.value,
-        password.current.value
-      )
-        .then((userCredential) => {
-          // Signed in
-          const user = userCredential.user;
-          console.log(user);
-          navigate('/browse')
-          // ...
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          console.log(errorCode, errorMessage);
           setErrMsg(errorCode);
         });
-       
+    } else {
+      // login
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrMsg(errorCode);
+        });
     }
   };
+
   return (
-    <div>
+    <div className="relative min-h-screen flex items-center justify-center">
       <Header />
-      <div className="absolute">
-        <img src={bgImage}></img>
+      <div className="absolute inset-0 overflow-hidden">
+        <img src={bgImage} className="w-full h-full object-cover" alt="Background" />
       </div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
         }}
-        className=" text-sm text-white absolute bg-black mx-[520px] my-[100px] px-12 py-16 rounded-lg bg-opacity-70"
+        className="text-sm text-white absolute bg-black mx-[5%] my-[5%] md:mx-[20%] md:my-[20%] lg:mx-[30%] lg:my-[30%] xl:mx-[40%] xl:my-[40%] 2xl:mx-[50%] 2xl:my-[50%] px-6 md:px-12 py-8 md:py-16 rounded-lg bg-opacity-70"
       >
         <h1 className="mb-6 font-bold text-xl">
           {isSignIn ? "SIGN IN" : "Sign UP"}
         </h1>
         {!isSignIn && (
           <input
-            className="rounded-sm mb-6 px-10 py-3 bg-gray-700"
+            className="rounded-sm mb-6 px-4 md:px-10 py-2 md:py-3 bg-gray-700"
             type="text"
             placeholder="Enter Name"
             ref={name}
           />
         )}
         <input
-          className="rounded-sm mb-6 px-10 py-3  bg-gray-700"
+          className="rounded-sm mb-6 px-4  md:px-10 py-2 md:py-3 bg-gray-700"
           type="text"
           placeholder="Enter Email"
           ref={email}
-        ></input>
-        <br></br>
+        />
+        <br />
         <input
-          className="rounded-sm py-3 px-10 bg-gray-700"
+          className="rounded-sm py-2 md:py-3 px-4 md:px-10 bg-gray-700"
           type="password"
           placeholder="Enter Password"
           ref={password}
-        ></input>
-        <br></br>
+        />
+        <br />
         <p className="text-red-800 font-bold text-lg mt-4">{errMsg}</p>
         <button
           onClick={submitHandler}
-          className="rounded-sm py-3 px-8 mt-6 w-full bg-red-600 mb-4"
+          className="rounded-sm py-2 md:py-3 px-4 md:px-8 mt-4 md:mt-6 w-full bg-red-600 mb-2 md:mb-4"
         >
-          {isSignIn ? "Sign In" : "SIgn UP"}
+          {isSignIn ? "Sign In" : "Sign Up"}
         </button>
         <p className="cursor-pointer" onClick={signInHandler}>
-          {isSignIn ? "New to Netflix? Sign UP" : "Already a user Sign IN"}
+          {isSignIn ? "New to Netflix? Sign Up" : "Already a user? Sign In"}
         </p>
       </form>
     </div>
